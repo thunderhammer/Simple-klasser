f229c91e1a0d609b075e4d1c9f6bce850f924365
Morten Engemand <engemand@hotmail.com>
2018-05-15 16:09:19 +0200

    Done later

--- Simple klasser/Program.cs
+++ Simple klasser/Program.cs
@@ -8,8 +8,55 @@ namespace Simple_klasser
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Person
         {
+            public string Fornavn;
+            public string Efternavn;
+            public int Fødselsår;
+
+
+            public Person()
+            {
+                Fornavn = "";
+                Efternavn = "";
+            }
+
+            public Person(string Fornavn, string Efternavn, int Fødselsår)
+            {
+                this.Fornavn = Fornavn.ToUpper();
+                this.Efternavn = Efternavn.ToUpper(); 
+                this.Fødselsår = Fødselsår;
+            }
+            
+            public string FuldtNavn()
+            {
+                return (Fornavn + " " + Efternavn);
+            }
+
+            public int Alder()
+            {
+                DateTime Current_time = DateTime.Now;
+                int estAlder = Current_time.Year - Fødselsår; 
+                return estAlder;
+            } 
+
+         /*   public void udskriv(string ptype)
+            {
+                Console.WriteLine(ptype.Fornavn);
+                )
+            } */
+
+        }
+            static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person("Jens", "Hansen", 1966);
+            Console.WriteLine(p1.Fornavn);
+            Console.WriteLine(p1.Efternavn);
+            Console.WriteLine(p2.Fornavn);
+            Console.WriteLine(p2.Efternavn);
+            Console.WriteLine(p2.Fødselsår);
+            Console.WriteLine(p2.Alder());
         }
     }
 }
